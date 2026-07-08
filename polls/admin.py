2bd5a6a7a2f09fe3298bf22eecfaafe8c190a212
rob <rob_vandam@gmx.com>
2026-07-08 09:22:48 +0200

    Toevoegen admin page voor Polls met een inline optie voor de choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "owner", "pub_date", "archived")
+    list_filter = ("archived", "pub_date")
+    search_fields = ("question_text",)
+    inlines = [ChoiceInline]
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("choice_text", "question", "votes")
