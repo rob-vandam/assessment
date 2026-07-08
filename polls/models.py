15444036ddb68bfd7c41e3e7a429a11573802421
rob <rob_vandam@gmx.com>
2026-07-08 09:19:44 +0200

    Some modifications to the model

--- polls/models.py
+++ polls/models.py
@@ -1,22 +1,23 @@
 from django.db import models
-
-from django.db import models
-
+from django.conf import settings
 
 class Question(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="questions")
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    archived = models.BooleanField(default=False)
     def __str__(self):
         return self.question_text
     class Meta:
         verbose_name_plural = "Questions"
+        ordering = ["-pub_date"]
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name="choices", on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
     class Meta:
         verbose_name_plural = "Choices"
-
