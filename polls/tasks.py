1047e071d0bf3c148849f9e986ca5271e176e716
rob <rob_vandam@gmx.com>
2026-07-08 10:33:33 +0200

    Het management command werkt niet zonder parameters

--- polls/tasks.py
+++ polls/tasks.py
@@ -4,4 +4,6 @@ from django.core.management import call_command
 
 @shared_task
 def run_api_test():
-    call_command("test_api")
+    call_command("test_api",
+    username='test1',
+    password='supersecret')
